f8672dbec8cb895a6c60e46e26e749490d50d2ae
Fco-Gabriel-Silva <fco.gabrielsilva2408@gmail.com>
2025-02-21 11:16:12 -0300

    code updated

---
+++ src/modules/schedules/show.js
@@ -0,0 +1,8 @@
+import dayjs from "dayjs";
+
+// Seleciona as sessões manhã, tarde e noite.
+const periodMorning = document.getElementById("period-morning");
+const periodAfternoon = document.getElementById("period-afternoon");
+const periodNight = document.getElementById("period-night");
+
+export function scheduleShow({ dailySchedules }) {}
